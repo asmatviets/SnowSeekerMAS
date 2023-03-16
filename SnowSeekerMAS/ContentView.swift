ef2745925cf531b1365f80cd474054eac6ed24b4
AndreyMatviets <asmatviets@gmail.com>
2023-03-16 09:29:19 +0400

    2 layouts from the box with NavigationView usage

--- SnowSeekerMAS/ContentView.swift
+++ SnowSeekerMAS/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Text("Hello world")
+                .navigationTitle("Primary")
+            
+            Text("Text on the second view")
         }
-        .padding()
     }
 }
 
