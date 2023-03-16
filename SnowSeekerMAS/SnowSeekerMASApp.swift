e0eeeb882830e8ce7fc3c70da3cc9c21a012ae8c
AndreyMatviets <asmatviets@gmail.com>
2023-03-16 09:12:29 +0400

    Initial Commit

---
+++ SnowSeekerMAS/SnowSeekerMASApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerMASApp.swift
+//  SnowSeekerMAS
+//
+//  Created by Andrey Matviets on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerMASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
